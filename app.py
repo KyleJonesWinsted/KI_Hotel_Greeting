5a7fda04accd9e657cc9a1954b72d0ddacf1b141
Kyle Jones <KyleJonesWinsted@gmail.com>
2020-02-11 21:06:20 -0600

    finished user interface

--- app.py
+++ app.py
@@ -4,19 +4,19 @@ from data_model.Template import Template
 from data_model.DataController import DataController
 from data_model.UserInterface import UserInterface
 
-ui = UserInterface()
-
 db = DataController()
 db.add_file_path('templates', './json/Templates.json')
 db.add_file_path('guests', './json/Guests.json')
 db.add_file_path('companies', './json/Companies.json')
 
-guests = db.get_all_guests()
-companies = db.get_all_companies()
-templates = db.get_all_templates()
-
 def main():
-    ui.main_menu(guests, companies, templates)
+    print("Press Control+C at any time to exit.\n\n")
+    guests = db.get_all_guests()
+    companies = db.get_all_companies()
+    templates = db.get_all_templates()
+    ui = UserInterface(guests, companies, templates)
+
+    ui.main_menu()
 
 if __name__ == "__main__":
     main()
--- data_model/UserInterface.py
+++ data_model/UserInterface.py
@@ -1,3 +1,6 @@
+import sys
+from data_model.Template import Template
+
 class UserInterface(object):
 
     def __init__(self, guests, companies, templates):
@@ -5,10 +8,14 @@ class UserInterface(object):
         self.companies = companies
         self.templates = templates
 
-    def get_user_int_input(self, user_prompt, menu, number_of_options):
+        self.current_greeting = None
+        self.current_guest = None
+        self.current_company = None
+
+    def _get_user_int_input(self, user_prompt, menu, number_of_options):
         try:
             user_input = int(input(user_prompt))
-            if user_input > number_of_options:
+            if user_input > number_of_options or user_input < 1:
                 raise Exception
             return user_input
         except Exception:
@@ -16,7 +23,10 @@ class UserInterface(object):
             menu()
         except KeyboardInterrupt:
             print('\n')
-            return
+            if menu == self.main_menu:
+                sys.exit()
+            else:
+                self.main_menu()
 
     def main_menu(self):
         print("\nWelcome! Please select an option below.")
@@ -24,22 +34,77 @@ class UserInterface(object):
             "Select Preset Greeting",
             "Send Custom Greeting"
         ])
-        user_input = self.get_user_int_input("Enter a number: ", self.main_menu, 2)
+        user_input = self._get_user_int_input("> ", self.main_menu, 2)
         if user_input == 1:
-            send_greeting()
+            self._select_greeting()
         elif user_input == 2:
-            create_greeting()
+            self._custom_greeting()
 
-    def send_greeting(self):
-        print("Select the greeting you would like to send.")
+    def _select_greeting(self):
+        print("\nSelect the greeting you would like to send.")
         self._enumerate_options([
-            template. for template in self.templates
+            template.shortname for template in self.templates
         ])
+        user_input = self._get_user_int_input("> ", self._select_greeting, len(self.templates))
+        self.current_greeting = self.templates[user_input - 1]
+        self._select_company()
 
-    def create_greeting(self):
-        pass
+    def _custom_greeting(self):
+        print("\nEnter your custom greeting. You can use the following placeholders: ")
+        placeholders = {
+            "timeOfDay": "Inserts Morning, Evening, or Afternoon",
+            "firstName": "Inserts Guest's First Name",
+            "lastName": "Inserts Guest's Last Name",
+            "roomNumber": "Inserts the Reserved Room Number",
+            "companyName": "Inserts the Hotel's Name"
+        }
+        for key, value in placeholders.items():
+            print("[{}] - {}".format(key, value))
+        print("\nBe sure to include the square brackets in your greeting placeholders.\n")
+        try:
+            user_input = input("> ")
+        except KeyboardInterrupt:
+            self.main_menu()
+        custom_template = Template(
+            id = 99,
+            shortname = "Custom Greeting",
+            template_text = user_input.strip()
+        )
+        self.current_greeting = custom_template
+        self._select_company()
+
+
+    def _select_company(self):
+        print("Select your company: ")
+        self._enumerate_options([
+            company.company for company in self.companies
+        ])
+        user_input = self._get_user_int_input("> ", self._select_company, len(self.companies))
+        self.current_company = self.companies[user_input - 1]
+        self._select_guest()
+
+    def _select_guest(self):
+        print("Select the guest to greet: ")
+        self._enumerate_options([
+            guest.full_name() for guest in self.guests
+        ])
+        user_input = self._get_user_int_input("> ", self._select_guest, len(self.guests))
+        self.current_guest = self.guests[user_input - 1]
+        self._send_greeting()
+
+    def _send_greeting(self):
+        print("\nSent greeting to {}\n".format(self.current_guest.full_name()))
+        print(self.current_greeting.render_template_string(self.current_guest, self.current_company))
+        
+        self.current_company = None
+        self.current_guest = None
+        self.current_greeting = None
+
+        self.main_menu()
 
 
     def _enumerate_options(self, options):
+        print("")
         for i, option in enumerate(options):
             print("[ {} ] {}".format(i + 1, option))
+        print("")
